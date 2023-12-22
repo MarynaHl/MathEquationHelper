2f221611d76f907f89fbe893762efaf54b70bb76
hlushak <75501308+MarynaHl@users.noreply.github.com>
2023-12-22 23:11:54 +0000

    upd

--- src/main/java/com/example/equations/database/DatabaseInitializer.java
+++ src/main/java/com/example/equations/database/DatabaseInitializer.java
@@ -12,11 +12,11 @@ public class DatabaseInitializer {
 
     public static void createTables() throws SQLException {
         try (Connection connection = DatabaseManager.getConnection();
-            Statement statement = connection.createStatement()) {
+                Statement statement = connection.createStatement()) {
             String createTableQuery = "CREATE TABLE IF NOT EXISTS equations ("
-                + "id INT AUTO_INCREMENT PRIMARY KEY,"
-                + "equation VARCHAR(255) NOT NULL UNIQUE ,"
-                + "roots VARCHAR(255) NOT NULL);";
+                    + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                    + "equation VARCHAR(255) NOT NULL UNIQUE ,"
+                    + "roots VARCHAR(255) NOT NULL);";
             statement.executeUpdate(createTableQuery);
         } catch (SQLException e) {
             throw new SQLException("Failed to create tables.");
@@ -24,3 +24,28 @@ public class DatabaseInitializer {
     }
 }
 
+/**
+ * Клас DatabaseInitializer відповідає за ініціалізацію бази даних для
+ * зберігання інформації про рівняння.
+ * У цьому класі використовується шаблон "Singleton" (приватний конструктор та
+ * публічний статичний метод),
+ * щоб гарантувати, що екземпляр класу буде створений лише один раз.
+ * 
+ * Метод createTables викликається для створення таблиці "equations" у базі
+ * даних, якщо така таблиця ще не існує.
+ * Використовується автоматичне управління ресурсами (try-with-resources) для
+ * забезпечення безпечного закриття
+ * з'єднання та оператора Statement після виконання запиту.
+ * 
+ * Запит для створення таблиці містить колонки id (ідентифікатор), equation
+ * (рівняння) та roots (корені рівняння).
+ * Колонка id є первинним ключем та автоматично збільшується при додаванні
+ * нового запису.
+ * Колонки equation та roots призначені для зберігання рядків з відповідними
+ * даними.
+ * 
+ * У випадку помилки створення таблиці, виникає SQLException, і викликаються
+ * відповідні обробники винятків,
+ * які викидають новий виняток з підходящим повідомленням про невдалу
+ * ініціалізацію бази даних.
+ */
--- src/main/java/com/example/equations/database/DatabaseManager.java
+++ src/main/java/com/example/equations/database/DatabaseManager.java
@@ -14,7 +14,6 @@ public class DatabaseManager {
     private static final String DB_USER = DBConfig.getDbUsername();
     private static final String DB_PASSWORD = DBConfig.getDbPassword();
 
-
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
@@ -28,4 +27,30 @@ public class DatabaseManager {
         }
     }
 }
+
+/**
+ * Клас DatabaseManager відповідає за управління підключенням до бази даних.
+ * У класі використовується шаблон "Singleton" (приватний конструктор та
+ * приватний статичний об'єкт),
+ * щоб гарантувати, що підключення до бази даних буде відкрито лише один раз та
+ * буде використовуватися
+ * для всіх операцій взаємодії з базою даних.
+ * 
+ * Конструктор та методи визначені як приватні, що унеможливлює створення нових
+ * екземплярів класу
+ * і забезпечує доступ до підключення лише через статичні методи класу.
+ * 
+ * Клас містить статичні поля для URL бази даних, імені користувача та пароля,
+ * які отримуються з класу
+ * DBConfig, що, ймовірно, містить конфігураційні дані для бази даних.
+ * 
+ * Метод getConnection повертає з'єднання до бази даних. Якщо з'єднання ще не
+ * створено або закрито, воно
+ * створюється за допомогою DriverManager.getConnection з використанням
+ * параметрів DB_URL, DB_USER та DB_PASSWORD.
+ * 
+ * Метод closeConnection закриває з'єднання до бази даних, якщо воно відкрите.
+ * Викликається, наприклад, при завершенні роботи
+ * з базою даних для звільнення ресурсів.
+ */
 
