081d74a2be1e2f0dc57f22f0509c141ea1efdd85
hlushak <75501308+MarynaHl@users.noreply.github.com>
2023-12-22 23:06:17 +0000

    upd

--- src/test/java/com/example/equations/EquationSolverTest.java
+++ src/test/java/com/example/equations/EquationSolverTest.java
@@ -20,14 +20,40 @@ class EquationSolverTest {
 
     static Stream<Arguments> equationValueProvider() {
         return Stream.of(
-            Arguments.of("2*x + 6 = 18", 6.0),
-            Arguments.of("x*x - 5x + 6 = 0", 2.0),
-            Arguments.of("x*x - 5x + 6 = 0", 3.0),
-            Arguments.of("x - 5x + 6 = -2", 2.0),
-            Arguments.of("6 + 6 = 2 * x", 6.0),
-            Arguments.of("15.6 / 2.0 + 2.0 = 2.0 + x", 7.8),
-            Arguments.of("16.4 / (2.0 + 2.0) = 2.0 + x", 2.1)
-        );
+                Arguments.of("2*x + 6 = 18", 6.0),
+                Arguments.of("x*x - 5x + 6 = 0", 2.0),
+                Arguments.of("x*x - 5x + 6 = 0", 3.0),
+                Arguments.of("x - 5x + 6 = -2", 2.0),
+                Arguments.of("6 + 6 = 2 * x", 6.0),
+                Arguments.of("15.6 / 2.0 + 2.0 = 2.0 + x", 7.8),
+                Arguments.of("16.4 / (2.0 + 2.0) = 2.0 + x", 2.1));
     }
 }
 
+/**
+ * Клас EquationSolverTest є класом для тестування функціоналу класу
+ * EquationSolver,
+ * який вирішує математичні рівняння та обчислює їх значення для конкретного
+ * значення x.
+ * 
+ * У цьому класі використовуються методи JUnit 5 для написання тестів:
+ * - @ParameterizedTest та @MethodSource для параметризованих тестів.
+ * - assertEquals для перевірки того, що обчислене значення рівне нулю з заданою
+ * точністю.
+ * 
+ * Метод testCalculateEquationValue викликає метод calculateEquationValue класу
+ * EquationSolver,
+ * передає йому рівняння та значення x, а потім перевіряє, що результат
+ * обчислення
+ * близький до нуля з точністю 1e-9 (дев'ять знаків після коми).
+ * 
+ * Метод equationValueProvider є постачальником даних для параметризованих
+ * тестів.
+ * Він надає різні рівняння та значення x, для яких очікується нульовий
+ * результат.
+ * 
+ * Приклади рівнянь та значень x включаються в потік Arguments, який передається
+ * в метод
+ * calculateEquationValue для проведення обчислень та перевірки результату
+ * тесту.
+ */
--- src/test/java/com/example/equations/EquationValidatorTest.java
+++ src/test/java/com/example/equations/EquationValidatorTest.java
@@ -8,69 +8,100 @@ import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Клас для тестування валідатора математичних рівнянь.
+ */
 class EquationValidatorTest {
 
+    /**
+     * Постачальник правильних виразів із дужками.
+     */
     static Stream<String> validParenthesesProvider() {
         return Stream.of(
-            "2*(x+5)",
-            "(2*x+5)",
-            "(2*5)+(x+5)",
-            "2*(x+5)-5+(x+5)",
-            "2*((x+5)-5)+(x+5)"
-        );
+                "2*(x+5)",
+                "(2*x+5)",
+                "(2*5)+(x+5)",
+                "2*(x+5)-5+(x+5)",
+                "2*((x+5)-5)+(x+5)");
     }
 
+    /**
+     * Постачальник неправильних виразів із дужками.
+     */
     static Stream<String> invalidParenthesesProvider() {
         return Stream.of(
-            "2*(x+5",
-            "(2*)x+5)",
-            "2*(x+5))",
-            "2*(((x+5))(x+2)",
-            "(2*((x+5))",
-            "2*(x+5))(x+2)"
-        );
+                "2*(x+5",
+                "(2*)x+5)",
+                "2*(x+5))",
+                "2*(((x+5))(x+2)",
+                "(2*((x+5))",
+                "2*(x+5))(x+2)");
     }
 
+    /**
+     * Постачальник правильних математичних виразів.
+     */
     static Stream<String> validExpressionProvider() {
         return Stream.of(
-            "2*x+5=10",
-            "-1.3*5/x=1.2",
-            "2*x=10",
-            "2*x+5+x+5=10",
-            "17=2*x+5"
-        );
+                "2*x+5=10",
+                "-1.3*5/x=1.2",
+                "2*x=10",
+                "2*x+5+x+5=10",
+                "17=2*x+5");
     }
 
+    /**
+     * Постачальник неправильних математичних виразів.
+     */
     static Stream<String> invalidExpressionProvider() {
         return Stream.of(
-            "2*x+*5=10",
-            "2*x+=",
-            "2*x+5+x+5=10+",
-            "17=2*x+5/0",
-            "2*x+5++x=10",
-            "x*3-(2+5)=",
-            "2*x+5-(x=10)"
-        );
+                "2*x+*5=10",
+                "2*x+=",
+                "2*x+5+x+5=10+",
+                "17=2*x+5/0",
+                "2*x+5++x=10",
+                "x*3-(2+5)=",
+                "2*x+5-(x=10)");
     }
 
+    /**
+     * Тест для перевірки правильності дужок у виразі.
+     *
+     * @param equation Математичний вираз
+     */
     @ParameterizedTest
     @MethodSource("validParenthesesProvider")
     void testValidParentheses(String equation) {
         assertTrue(EquationValidator.isParenthesesValid(equation));
     }
 
+    /**
+     * Тест для перевірки неправильності дужок у виразі.
+     *
+     * @param equation Математичний вираз
+     */
     @ParameterizedTest
     @MethodSource("invalidParenthesesProvider")
     void testInvalidParentheses(String equation) {
         assertFalse(EquationValidator.isParenthesesValid(equation));
     }
 
+    /**
+     * Тест для перевірки правильності математичного виразу.
+     *
+     * @param equation Математичний вираз
+     */
     @ParameterizedTest
     @MethodSource("validExpressionProvider")
     void testValidExpression(String equation) {
         assertTrue(EquationValidator.isExpressionValid(equation));
     }
 
+    /**
+     * Тест для перевірки неправильності математичного виразу.
+     *
+     * @param equation Математичний вираз
+     */
     @ParameterizedTest
     @MethodSource("invalidExpressionProvider")
     void testInvalidExpression(String equation) {
@@ -78,4 +109,27 @@ class EquationValidatorTest {
     }
 }
 
-
+/**
+ * У цьому коді:
+ * 
+ * Клас EquationValidatorTest є класом для тестування функціоналу класу
+ * EquationValidator,
+ * який, ймовірно, містить методи для валідації математичних виразів.
+ * 
+ * Кожен метод validParenthesesProvider, invalidParenthesesProvider,
+ * validExpressionProvider і invalidExpressionProvider є постачальником даних
+ * для параметризованих тестів. Вони надають різні математичні вирази,
+ * які повинні бути правильно або неправильно валідовані.
+ * 
+ * Кожен параметризований тест використовує анотації @ParameterizedTest
+ * та @MethodSource
+ * для вказівки постачальника даних і методу, який надає ці дані.
+ * 
+ * У тестах використовуються методи assertTrue та assertFalse для перевірки
+ * правильності
+ * або неправильності валідації математичних виразів.
+ * 
+ * Кожен тест викликає відповідні методи з класу EquationValidator для перевірки
+ * валідності
+ * дужок або математичного виразу.
+ */
