2de40ae9c3d664283b86b332728237604513675e
hlushak <75501308+MarynaHl@users.noreply.github.com>
2023-12-25 23:48:56 +0000

    Upd

--- src/main/java/com/example/equations/database/DatabaseManager.java
+++ src/main/java/com/example/equations/database/DatabaseManager.java
@@ -1,32 +1,32 @@
-// package com.example.equations.database;
+package com.example.equations.database;
 
-// import java.sql.Connection;
-// import java.sql.DriverManager;
-// import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-// public class DatabaseManager {
+public class DatabaseManager {
 
-//     private DatabaseManager() {
-//     }
+    private DatabaseManager() {
+    }
 
-//     private static Connection connection;
-//     private static final String DB_URL = DBConfig.getDbUrl();
-//     private static final String DB_USER = DBConfig.getDbUsername();
-//     private static final String DB_PASSWORD = DBConfig.getDbPassword();
+    private static Connection connection;
+    private static final String DB_URL = DBConfig.getDbUrl();
+    private static final String DB_USER = DBConfig.getDbUsername();
+    private static final String DB_PASSWORD = DBConfig.getDbPassword();
 
-//     public static Connection getConnection() throws SQLException {
-//         if (connection == null || connection.isClosed()) {
-//             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-//         }
-//         return connection;
-//     }
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        }
+        return connection;
+    }
 
-//     public static void closeConnection() throws SQLException {
-//         if (connection != null && !connection.isClosed()) {
-//             connection.close();
-//         }
-//     }
-// }
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+}
 
 // /**
 //  * Клас DatabaseManager відповідає за управління підключенням до бази даних.
